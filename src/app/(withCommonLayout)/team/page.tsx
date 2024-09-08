ca49c3ab7a2f2c3b831cc8efa52e12d168645582
FahimMuntashir1 <fahimasdf07@gmail.com>
2024-09-08 10:44:29 +0600

    version 0.1 done without test

--- src/app/(withCommonLayout)/team/page.tsx
+++ src/app/(withCommonLayout)/team/page.tsx
@@ -1,10 +1,12 @@
 import Adviser from "./Adviser";
+import Member from "./Member";
 import Moon from "./Moon";
 const Team = () => {
   return (
     <div>
       <Moon></Moon>
       <Adviser></Adviser>
+      <Member></Member>
     </div>
   );
 };
--- src/services/actions/faculty.tsx
+++ src/services/actions/faculty.tsx
@@ -1,17 +1,17 @@
 'use server'
 
 export const getFacultyProfileInfo = async (id: string) => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/faculty-details/${id}`, {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/users/faculty-details/${id}`, {
         cache: 'no-store',
         method: 'GET',
         credentials: 'include',
     });
 
     const result = await response.json();
-    return result.data;
+    return result.data[0];
 }
 export const getAllFaculty = async () => {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/all-faculty/`, {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_API_URL}/users/all-faculty`, {
         cache: 'no-store',
         method: 'GET',
         credentials: 'include',
